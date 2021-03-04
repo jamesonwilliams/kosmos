ba2ea8b3a10dbc346d31898a86567dc1f728c29d
Jameson Williams <jhwill@amazon.com>
2021-03-04 02:15:00 -0600

    Fix lint issues in JSONObjectExt

--- cognito-client/src/main/java/org/nosemaj/kosmos/cognito/JSONObjectExt.kt
+++ cognito-client/src/main/java/org/nosemaj/kosmos/cognito/JSONObjectExt.kt
@@ -3,5 +3,5 @@ package org.nosemaj.kosmos.cognito
 import org.json.JSONObject
 
 fun JSONObject.maybeString(key: String): String? {
-    return if (isNull(key)) null else optString(key, null)
+    return if (isNull(key)) null else getString(key)
 }
